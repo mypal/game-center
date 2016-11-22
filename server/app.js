0dde6de9d3d20e3b486caff961b56dbf0f99bcfc
shancheng.wang <shancheng.wang@dianping.com>
2016-11-22 21:39:40 +0800

    test socket.io

--- server/app.js
+++ server/app.js
@@ -1,8 +1,33 @@
-var koa = require('koa');
+const koa = require('koa'),
+    mount = require('koa-mount'),
+    router = require('koa-router')(),
+    logger = require('koa-logger'),
+    render = require('koa-swig'),
+    path = require('path');
+
 var app = koa();
 
-app.use(function *(){
-    this.body = 'Hello World';
+app.context.render = render({
+    root: path.join(__dirname, '../src/static/'),
+    autoescape: false,
+    cache: false,
+    ext: 'html'
+});
+app.use(logger());
+
+var index = function *() {
+    yield* this.render('index');
+};
+
+router.get('/', index);
+app.use(router.routes()).use(router.allowedMethods());
+
+const server = require('http').createServer(app.callback()),
+    io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+    console.log('connect');
+    socket.broadcast.emit('talk', 'connect');
 });
 
-app.listen(8080);
+server.listen(8080);
